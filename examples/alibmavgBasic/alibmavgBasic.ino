ad0f1c666a6d9bc01cdd385ddbb4513ae47367d1
Martin Legleiter <martin.legleiter@gmail.com>
2023-08-23 00:54:32 +0200

    Fixed the basic example

--- examples/alibmavgBasic/alibmavgBasic.ino
+++ examples/alibmavgBasic/alibmavgBasic.ino
@@ -20,8 +20,8 @@
 
 SMA_FUNCTION( long, SMAFunction1,  8 );
 SMA_FUNCTION( long, SMAFunction2, 64 );
-SMA_FUNCTION( long, WMAFunction1,  8 );
-SMA_FUNCTION( long, WMAFunction2, 64 );
+WMA_FUNCTION( long, WMAFunction1,  8 );
+WMA_FUNCTION( long, WMAFunction2, 64 );
 
 /*-----------------------------------------------------------*/
 
@@ -35,15 +35,17 @@ void loop( void )
 {
     long randomValue = random( -100, 101 );
 
+    Serial.print( "random_value:" );
     Serial.print( randomValue );
-    Serial.print( ',' );
+    Serial.print( ",SMA_8:" );
     Serial.print( SMAFunction1( randomValue ) );
-    Serial.print( ',' );
+    Serial.print( ",SMA_64:" );
     Serial.print( SMAFunction2( randomValue ) );
-    Serial.print( ',' );
+    Serial.print( ",WMA_8:" );
     Serial.print( WMAFunction1( randomValue ) );
-    Serial.print( ',' );
-    Serial.println( WMAFunction2( randomValue ) );
+    Serial.print( ",WMA_64:" );
+    Serial.print( WMAFunction2( randomValue ) );
+    Serial.println( ",-100:-100,0:0,100:100" );
 
-    delay( 20 );
+    delay( 50 );
 }
